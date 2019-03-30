572db0731b134ec74a7564312e2ee8591851a7b2
Joseph Reilly <reilly4181@gmail.com>
2019-03-30 01:42:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ function theBeatlesPlay(musicians, instruments) {
     return allMusicians;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(arr) {
+  var result= []; 
+  var i = 0; 
+  while(i < arr.length) {
+    result.push(arr[i] + '!!!')
+    i++; 
   }
-  return array;
+  return result; 
 }
+
+johnLennonFacts(facts); 
+
+
+
+
 function iLoveTheBeatles(num) {
     var love = [];
     do {
