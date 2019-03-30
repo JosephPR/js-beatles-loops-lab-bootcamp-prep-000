ffa1ea7cbd8cd8da3dd5ad0fc4146541d7052407
Joseph Reilly <reilly4181@gmail.com>
2019-03-30 01:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,22 @@ function johnLennonFacts(arr) {
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
+ var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
+}
 
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
 }
